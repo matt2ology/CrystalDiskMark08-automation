f356c2e1f68643d955ee6fe48a1d38b0fd8ab0a2
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-08-14 12:50:54 -0700

    Deleted reworking logic

--- main.py
+++ main.py
@@ -23,39 +23,6 @@ logging.basicConfig(
 )
 
 
-def check_default_install_path():
-    default_install_path = r'C:\Program Files\CrystalDiskMark8'
-    is_default_install = False
-    logging.info("Installed via default path? " + default_install_path)
-    if os.path.isdir(default_install_path):
-        logging.info("Application is installed in default directory")
-        is_default_install = True
-    else:
-        logging.error("Application IS NOT installed in default directory!")
-    return is_default_install
-    pass
-
-
-def is_in_default_path(is_default_install_path):
-    logging.INFO(
-        "Please uninstall and re-install via default directory path")
-    logging.INFO("Now closing automation...")
-    sys.exit()
-    pass
-
-
-def launchCDM():
-    versionx64 = r"C:\Program Files\CrystalDiskMark8\DiskMark64.exe"
-    versionx32 = r"C:\Program Files\CrystalDiskMark8\DiskMark32.exe"
-    if os.path.isfile(versionx64):
-        logging.info("Opeing " + versionx64)
-        subprocess.Popen(versionx64)
-    else:
-        logging.info("Opeing " + versionx32)
-        subprocess.Popen(versionx32)
-    pass
-
-
 def main():
     logging.info("Starting Crystal Disk Mark Automation...")
     is_in_default_path(check_default_install_path())
